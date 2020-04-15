7797d549462041f52f14769293743c3477b40ff1
jerperez <jerperez@starbucks.com>
2020-04-14 20:57:20 -0700

    Added low and high to weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+            + " The high for today is " + body.daily.data[0].temperatureHigh + "."
+            + " The low for today is " + body.daily.data[0].temperatureLow + ".")
         }
     })
 }
